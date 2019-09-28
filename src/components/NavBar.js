0139950f19e8efd3768ca19b53ffde0dc723265f
github username <github email address>
2019-09-28 12:13:36 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar(props){
     <nav className={`navbar ${colors[props.color]}`}>
       <div className='container-fluid'>
         <div className='navbar-header'>
-          <a className='navbar-brand'>
+          <a className='navbar-brand'> 
             { props.title }
           </a>
         </div>
